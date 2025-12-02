8464ab98f5e9d56c2e079f921bf453e4b81cc687
nazmul-haque-nihal <184433790+nazmul-haque-nihal@users.noreply.github.com>
2025-12-03 01:53:24 +0600

    Delete utils directory

--- utils/postprocess.py
+++
@@ -1,4 +0,0 @@
-def postprocess_detections(detections, original_size, input_size=(640, 640)):
-    """Convert normalized coordinates to original image coordinates"""
-    # YOLOv5 outputs are already in pixel coordinates
-    return detections
